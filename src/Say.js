398a4ef812c818366f2ed3049f5bcf9bcf19a53a
jinnyRyu <hajinryu51@gmail.com>
2022-09-04 15:38:47 +0900

    use state

--- src/Say.js
+++ src/Say.js
@@ -5,13 +5,15 @@ const Say = () => {
     const onClickEnter = () => setMessage('안녕');
     const onClickLeave = () => setMessage('잘가');
 
-
+    const [color, setColor] = useState('black');
     return (
 
         <div>
             <button onClick={onClickEnter}>입장</button>
-            <button onClick={onClickLeave}>입장</button>
-            <h1>{message}</h1>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1 style={{ color }}>{message}</h1>
+
+            <button style={{ color: 'green' }} onClick={() => setColor('green')}>초록</button>
         </div>
 
     )
