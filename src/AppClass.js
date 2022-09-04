931be3cee6726244e1a8ba242e9bc193c6730f60
jinnyRyu <hajinryu51@gmail.com>
2022-09-04 13:37:36 +0900

    class component

---
+++ src/AppClass.js
@@ -0,0 +1,10 @@
+import React, { Conponent } from "react";
+
+class AppClass extends Conponent {
+    render() {
+        const name = 'react';
+        return <div className="react">{name}</div>
+    }
+}
+
+export default AppClass;
