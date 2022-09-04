34a24e67f9020bcda86ad6c292fe68fe4f8f949c
jinnyRyu <hajinryu51@gmail.com>
2022-09-04 15:15:11 +0900

    초기값 셋팅시 생성자를 만들지 않고 셋팅하는 방법

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {// 컴포넌트에 state 설정할땐 생성자에 
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: 0
-        };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         number: 0,
+    //         fixNumber: 0
+    //     };
+    // }
+    state = {
+        number: 0,
+        fixNumber: 0
     }
     render() {
-        const { number } = this.state;
+        const { number, fixNumber } = this.state;
         return (
             <div>
                 <h1>{number}</h1>
+                <h1> 바뀌지 않는 값 {fixNumber}</h1>
                 <button onClick={() =>
                     this.setState({ number: number + 1 })}>+1</button>
             </div>
